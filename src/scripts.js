7c181d6f83611cc53ef1aa2a4aea62c98e4c110b
Laura Guerra <l.garciaguerra1@gmail.com>
2023-05-24 18:31:30 -0400

    Add sample data and beginning assets

--- src/scripts.js
+++ src/scripts.js
@@ -3,9 +3,15 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
+import { getBookingsData, getRoomsData } from './apicalls';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import './images/home-icon.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
+window.addEventListener('load', () => {
+  getRoomsData()
+  getBookingsData()
+})
