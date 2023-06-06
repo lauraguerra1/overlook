c944f5c3cf0c3a06a054171967e7aedbd00cc6cd
Laura Guerra <l.garciaguerra1@gmail.com>
2023-06-06 17:57:41 -0400

    Style modal layout

--- src/scripts.js
+++ src/scripts.js
@@ -7,7 +7,7 @@ import { getBookingsData, getRoomsData } from './apicalls';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/cabana-room.png'
-import './images/lotus-logo.png'
+import './images/lotus-logo-b.png'
 import './images/pool-side.png'
 import './images/ocean-view.png'
 
