fd0ccb6fa467b7fa46a223514795dc3c0072a08e
Laura Guerra <l.garciaguerra1@gmail.com>
2023-06-07 11:37:10 -0400

    Make modal open and close

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,13 +1,32 @@
-import {leftBudgetValue, rightBudgetValue } from './scripts'
+import {
+  leftBudgetValue,
+  rightBudgetValue,
+  filterBtn,
+  filterModal,
+  availableRoomsView,
+} from './scripts';
 
 const slideBudget = (e) => {
   const targets = {
     firstSlider: leftBudgetValue,
-    secondSlider: rightBudgetValue
-  }
-  targets[e.target.id].innerText = e.target.value;
-}
+    secondSlider: rightBudgetValue,
+  };
 
-export {
-  slideBudget
-}
+  targets[e.target.id].innerText = e.target.value;
+};
+
+const openModal = () => {
+  filterBtn.disabled = true;
+  filterBtn.classList.add('no-click');
+  availableRoomsView.classList.add('blur', 'no-click');
+  filterModal.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  filterBtn.disabled = false;
+  filterBtn.classList.remove('no-click');
+  availableRoomsView.classList.remove('blur', 'no-click');
+  filterModal.classList.add('hidden');
+};
+
+export { slideBudget, openModal, closeModal };
--- src/scripts.js
+++ src/scripts.js
@@ -1,34 +1,48 @@
 // IMPORTS
 import './css/styles.css';
 import { getBookingsData, getRoomsData } from './apicalls';
-import { slideBudget } from './domUpdates'
+import { slideBudget, openModal, closeModal } from './domUpdates';
 
 //IMAGES
-import './images/cabana-room.png'
-import './images/lotus-logo-b.png'
-import './images/pool-side.png'
-import './images/ocean-view.png'
+import './images/cabana-room.png';
+import './images/lotus-logo-b.png';
+import './images/pool-side.png';
+import './images/ocean-view.png';
 
-//GLOBAL VARIABLES 
-const leftSlider = document.querySelector('#firstSlider')
-const rightSlider = document.querySelector('#secondSlider')
-const leftBudgetValue = document.querySelector('.value1')
-const rightBudgetValue = document.querySelector('.value2')
+//GLOBAL VARIABLES
+const leftSlider = document.querySelector('#firstSlider');
+const rightSlider = document.querySelector('#secondSlider');
+const leftBudgetValue = document.querySelector('.value1');
+const rightBudgetValue = document.querySelector('.value2');
+const closeBtn = document.querySelector('.close-btn');
+const filterBtn = document.querySelector('.filter-button');
+const filterModal = document.querySelector('.filter-modal');
+const availableRoomsView = document.querySelector('.available-rooms-view')
 
-
-// EVENT LISTENERS 
+// EVENT LISTENERS
 window.addEventListener('load', () => {
-  getRoomsData()
-  getBookingsData()
+  getRoomsData();
+  getBookingsData();
   // console.log(leftSlider, rightSlider)
-})
+});
 
-Array.from([leftSlider, rightSlider]).forEach(input => {
-  console.log(input)
-  input.addEventListener('input', (e) => {slideBudget(e)})
-})
+Array.from([leftSlider, rightSlider]).forEach((input) => {
+  console.log(input);
+  input.addEventListener('input', (e) => {
+    slideBudget(e);
+  });
+});
 
-export {
-  leftBudgetValue,
-  rightBudgetValue
-}
+filterBtn.addEventListener('click', openModal);
+filterBtn.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    openModal()
+  }
+})
+closeBtn.addEventListener('click', closeModal)
+closeBtn.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    closeModal()
+  }
+})
+export { leftBudgetValue, rightBudgetValue, filterBtn, filterModal, availableRoomsView };
